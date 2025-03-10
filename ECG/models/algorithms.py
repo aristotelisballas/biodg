5a056acc3b0ce96f37c596bf9979d578b1444060
Aristotelis Ballas <87428688+aristotelisballas@users.noreply.github.com>
2025-03-10 16:49:40 +0200

    Update algorithms.py

--- ECG/models/algorithms.py
+++ ECG/models/algorithms.py
@@ -124,42 +124,6 @@ class ERM(Algorithm):
         return self.network(x)
 
 
-# class ERM(Algorithm):
-#     """
-#     Empirical Risk Minimization (ERM)
-#     """
-#
-#     def __init__(self, input_shape, num_classes, num_domains, hparams, ):
-#         super(ERM, self).__init__(input_shape, num_classes, num_domains,
-#                                   hparams)
-#         self.featurizer = networks.Featurizer(input_shape, self.hparams)
-#
-#         self.classifier = networks.Classifier(
-#             self.featurizer.n_outputs,
-#             num_classes,
-#             self.hparams['nonlinear_classifier'])
-#
-#         self.network = nn.Sequential(self.featurizer, self.classifier)
-#         self.optimizer = torch.optim.Adam(
-#             self.network.parameters(),
-#             lr=self.hparams["lr"],
-#             weight_decay=self.hparams['weight_decay']
-#         )
-#
-#     def update(self, minibatches, unlabeled=None):
-#         all_x = torch.cat([x for x, y in minibatches])
-#         all_y = torch.cat([y for x, y in minibatches])
-#         loss = F.cross_entropy(self.predict(all_x), all_y.float())
-#
-#         self.optimizer.zero_grad()
-#         loss.backward()
-#         self.optimizer.step()
-#
-#         return {'loss': loss.item()}
-#
-#     def predict(self, x):
-#         return self.network(x)
-
 class Fish(Algorithm):
     """
     Implementation of Fish, as seen in Gradient Matching for Domain
